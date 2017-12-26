31ac0e4db3eda207935a801c3e0415022b17b43b
Yuji Takaesu <taka.esu.user@gmail.com>
2017-12-27 06:10:39 +0900

    Initialize, "bundle gem --exe --test=rspec excel-transpose"

---
+++ lib/excel/transpose.rb
@@ -0,0 +1,7 @@
+require "excel/transpose/version"
+
+module Excel
+  module Transpose
+    # Your code goes here...
+  end
+end
---
+++ lib/excel/transpose/version.rb
@@ -0,0 +1,5 @@
+module Excel
+  module Transpose
+    VERSION = "0.1.0"
+  end
+end
